a42e4f2cc77698ec58f080e5a7723dd8705e90db
K9-guardian <krishna.g10rules@gmail.com>
2022-02-11 13:06:11 -0800

    1st problem

---
+++ 0001_TwoSum/Solution.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+import java.util.HashMap;
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> m = new HashMap<>();
+        int[] result = null;
+
+        for (int i = 0; i < nums.length; i++)
+            if (m.containsKey(target - nums[i]))
+                result = new int[]{i, m.get(target - nums[i])};
+            else
+                m.put(nums[i], i);
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {2,7,11,15};
+        int target = 9;
+        Solution sc = new Solution();
+        System.out.println(Arrays.toString(sc.twoSum(nums, target)));
+    }
+}
