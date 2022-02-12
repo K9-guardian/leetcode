b334a71189e40af3caa0c3bf84850d48ec391d46
K9-guardian <krishna.g10rules@gmail.com>
2022-02-11 16:06:14 -0800

    4th problem

---
+++ 0011_ContainerWithMostWater.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int maxArea(int[] height) {
+        int start = 0, end = height.length - 1;
+        int area = 0;
+
+        while (start != end) {
+            int min = Math.min(height[start], height[end]);
+            area = Math.max(area, (end - start) * min);
+
+            if (min == height[start])
+                start++;
+            else
+                end--;
+        }
+
+        return area;
+    }
+
+    public static void main(String[] args) {
+        int[] height = {1,8,6,2,5,4,8,3,7};
+        Solution sc = new Solution();
+        System.out.println(sc.maxArea(height));
+    }
+}
