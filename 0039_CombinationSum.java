6f2e2c680835a30e233e52734dc5294f49fffb0d
K9-guardian <krishna.g10rules@gmail.com>
2022-03-02 01:21:16 -0800

    8th problem

---
+++ 0039_CombinationSum.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+class Solution {
+    record DFSElement(int i, int total, List<Integer> nums) { }
+
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> result = new ArrayList<>();
+        Deque<DFSElement> stack = new ArrayDeque<>();
+        stack.push(new DFSElement(0, 0, new ArrayList<>()));
+
+        while (!stack.isEmpty()) {
+            DFSElement e = stack.pop();
+
+            if (e.total == target) {
+                result.add(new ArrayList<>(e.nums));
+                continue;
+            }
+            if (e.i >= candidates.length || e.total > target)
+                continue;
+
+            List<Integer> s1 = new ArrayList<>(e.nums), s2 = new ArrayList<>(e.nums);
+            s2.add(candidates[e.i]);
+
+            stack.push(new DFSElement(e.i + 1, e.total, s1));
+            stack.push(new DFSElement(e.i, e.total + candidates[e.i], s2));
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] candidates = {2,3,6,7};
+        Solution sc = new Solution();
+        System.out.println(sc.combinationSum(candidates, 7));
+    }
+}
