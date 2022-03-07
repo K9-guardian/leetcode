d042a39b70f3b7e736d840f8bafbfe5602692b59
K9-guardian <girkar.km@gmail.com>
2022-03-07 01:56:01 -0800

    Add problem

---
+++ 0053_MaximumSubarray.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int maxSum = nums[0];
+        int sum = 0;
+
+        for (int i = 0; i < nums.length; i++)
+            if (sum < 0)
+                sum = 0;
+            else {
+                sum += nums[i];
+                maxSum = Math.max(sum, maxSum);
+            }
+
+        return maxSum;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {-2,1,-3,4,-1,2,1,-5,4};
+        Solution sc = new Solution();
+        System.out.println(sc.maxSubArray(nums));
+    }
+}
