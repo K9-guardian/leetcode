1873f235b9ce26f4370c0abb75cc0b89113517c0
K9-guardian <girkar.km@gmail.com>
2022-03-07 01:58:22 -0800

    I hate side effects

--- 0053_MaximumSubarray.java
+++ 0053_MaximumSubarray.java
@@ -3,13 +3,12 @@ class Solution {
         int maxSum = nums[0];
         int sum = 0;
 
-        for (int i = 0; i < nums.length; i++)
+        for (int i = 0; i < nums.length; i++) {
             if (sum < 0)
                 sum = 0;
-            else {
-                sum += nums[i];
-                maxSum = Math.max(sum, maxSum);
-            }
+            sum += nums[i];
+            maxSum = Math.max(sum, maxSum);
+        }
 
         return maxSum;
     }
