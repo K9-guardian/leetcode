a779369ff2a6b48e03a1613b3fae9fcfd69933f7
K9-guardian <girkar.km@gmail.com>
2022-03-13 00:51:29 -0800

    moving the goalpost

---
+++ 0055_JumpGame.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        int goalPost = nums.length - 1;
+
+        for (int pos = nums.length - 1; pos > -1; pos--) {
+            int dist = goalPost - pos;
+            if (nums[pos] >= dist)
+                goalPost = pos;
+        }
+
+        return goalPost == 0;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {2,3,1,1,4};
+        Solution sc = new Solution();
+        System.out.println(sc.canJump(nums));
+    }
+}
