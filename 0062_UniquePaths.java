c29aa251d43596b65660441bd29183bceaebbbb5
K9-guardian <girkar.km@gmail.com>
2022-07-04 09:30:48 -0700

    rename

--- 0062_UniquePaths.java
+++ 0062_UniquePaths.java
@@ -2,20 +2,20 @@ import java.util.*;
 
 class Solution {
     public int uniquePaths(int m, int n) {
-        int[][] dp = new int[m][n];
+        int[][] table = new int[m][n];
 
         // Precomputed paths for right column and bottom row.
-        dp[m - 1][n - 1] = 1;
+        table[m - 1][n - 1] = 1;
         for (int i = 0; i < n - 1; i++)
-            dp[m - 1][i] = 1;
+            table[m - 1][i] = 1;
         for (int i = 0; i < m - 1; i++)
-            dp[i][n - 1] = 1;
+            table[i][n - 1] = 1;
 
         for (int i = m - 2; i >= 0; i--)
             for (int j = n - 2; j >= 0; j--)
-                dp[i][j] = dp[i + 1][j] + dp[i][j + 1];
+                table[i][j] = table[i + 1][j] + table[i][j + 1];
 
-        return dp[0][0];
+        return table[0][0];
     }
 
     public static void main(String[] args) {
