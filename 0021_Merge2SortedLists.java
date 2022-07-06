ded13dc4abf1556f03fec6dc62b296955637acd6
K9-guardian <girkar.km@gmail.com>
2022-07-06 00:53:28 -0700

    dumb solve

---
+++ 0021_Merge2SortedLists.java
@@ -0,0 +1,32 @@
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode fst = list1, snd = list2;
+        ListNode dummy = new ListNode(0, null), merged = dummy;
+
+        while (fst != null && snd != null) {
+            if (fst.val < snd.val) {
+                merged.next = new ListNode(fst.val);
+                fst = fst.next;
+            }
+            else {
+                merged.next = new ListNode(snd.val);
+                snd = snd.next;
+            }
+            merged = merged.next;
+        }
+
+        while (fst != null) {
+            merged.next = new ListNode(fst.val);
+            fst = fst.next;
+            merged = merged.next;
+        }
+
+        while (snd != null) {
+            merged.next = new ListNode(snd.val);
+            snd = snd.next;
+            merged = merged.next;
+        }
+
+        return dummy.next;
+    }
+}
