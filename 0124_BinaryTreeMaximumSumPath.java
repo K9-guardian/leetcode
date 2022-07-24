4fb5eaebbcc6cfb84d3f8e7ae62f3b53f591d8f7
K9-guardian <girkar.km@gmail.com>
2022-07-24 02:26:12 -0700

    Finish lc

---
+++ 0124_BinaryTreeMaximumSumPath.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int maxPathSum(TreeNode root) {
+        int[] res = { Integer.MIN_VALUE };
+        maxPathSumRec(root, res);
+        return res[0];
+    }
+
+    public int maxPathSumRec(TreeNode node, int[] res) {
+        if (node == null) return 0;
+
+        int left = node.val + maxPathSumRec(node.left, res);
+        int right = node.val + maxPathSumRec(node.right, res);
+        int both = left + right - node.val;
+
+        int compMax = Math.max(node.val, Math.max(left, right));
+        res[0] = Math.max(compMax, Math.max(both, res[0]));
+        return compMax;
+    }
+}
