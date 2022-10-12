828b67eb3881ee943a99d0503127f62c3d937a3e
K9-guardian <girkar.km@gmail.com>
2022-10-11 20:57:32 -0700

    Finish lc

--- 1175_PrimeArrangements.java
+++ 1175_PrimeArrangements.java
@@ -7,16 +7,24 @@ class Solution {
         boolean[] primes = new boolean[n + 1];
         Arrays.fill(primes, true);
         primes[1] = false;
+
         int p = 2;
         while (p * p <= n) {
             for (int i = p * p; i <= n; i += p)
                 primes[i] = false;
-            do
-                p++;
-            while (p * p <= n && primes[p] == false);
+            do p++; while (p * p <= n && primes[p] == false);
         }
-        System.out.println(Arrays.toString(primes));
-        return 0;
+
+        int numPrimes = 0;
+        for (int i = 1; i < primes.length; i++)
+            if (primes[i])
+                numPrimes++;
+
+        long res = 1;
+        for (int i = numPrimes; i > 0; i--) res = (res * i) % MOD;
+        for (int i = n - numPrimes; i > 0; i--) res = (res * i) % MOD;
+
+        return (int) res;
     }
 
     public static void main(String[] args) {
