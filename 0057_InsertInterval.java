d92b17482e29528235a227afb5b915f6dc24bfd0
K9-guardian <girkar.km@gmail.com>
2022-10-19 10:30:30 -0700

    Better solve

--- 0057_InsertInterval.java
+++ 0057_InsertInterval.java
@@ -2,31 +2,28 @@ import java.util.*;
 
 class Solution {
     public int[][] insert(int[][] intervals, int[] newInterval) {
-        int i = Arrays.binarySearch(intervals, newInterval, (xs, ys) -> xs[1] - ys[0]);
-        int j = Arrays.binarySearch(intervals, newInterval, (xs, ys) -> xs[0] - ys[1]);
+        List<int[]> res = new ArrayList<>();
 
-        int fst = i < 0 ? ~i : i;
-        int snd = j < 0 ? ~j : j;
+        for (int i = 0; i < intervals.length; i++) {
+            if (newInterval[1] < intervals[i][0]) {
+                res.add(newInterval);
+                for (int j = i; j < intervals.length; j++) res.add(intervals[j]);
+                return res.stream().toArray(int[][]::new);
+            } else if (newInterval[0] > intervals[i][1])
+                res.add(intervals[i]);
+            else {
+                newInterval[0] = Math.min(newInterval[0], intervals[i][0]);
+                newInterval[1] = Math.max(newInterval[1], intervals[i][1]);
+            }
+        }
 
-        int[][] merged = new int[intervals.length - (snd - fst) + (j < 0 ? 1 : 0)][2];
-        System.arraycopy(intervals, 0, merged, 0, fst);
-        System.arraycopy(intervals, j < 0 ? ~j : j + 1, merged, fst + 1, merged.length - (fst + 1));
-
-        int lower = Math.min(newInterval[0],
-                             fst == intervals.length ? Integer.MAX_VALUE : intervals[fst][0]);
-        int upper = Math.max(newInterval[1],
-                             j < 0 && snd == 0
-                           ? Integer.MIN_VALUE
-                           : intervals[j < 0 ? snd - 1 : snd][1]);
-
-        merged[fst] = new int[] { lower, upper };
-
-        return merged;
+        res.add(newInterval);
+        return res.stream().toArray(int[][]::new);
     }
 
     public static void main(String[] args) {
-        int[][] intervals = {{1,5}};
-        int[] newInterval = {0,1};
+        int[][] intervals = { { 1, 5 } };
+        int[] newInterval = { 0, 1 };
         Solution sc = new Solution();
         System.out.println(Arrays.deepToString(sc.insert(intervals, newInterval)));
     }
