a3583c0bd04b0dd4b05f78077d31f6bf66201faf
Krishna Girkar <girkar.km@gmail.com>
2025-12-16 01:22:43 -0800

    finish problem

--- 0122_best_time_to_buy_and_sell_stock_II.py
+++ 0122_best_time_to_buy_and_sell_stock_II.py
@@ -1,5 +1,21 @@
+#!/usr/bin/env python
 from typing import List
 
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        return 0
+        i, j = 0, 0
+        m = 0
+        prices.append(0)
+        while j < len(prices):
+            if prices[j-1] >= prices[j]:
+                m += prices[j-1] - prices[i]
+                i = j
+            j += 1
+        return m
+
+s = Solution()
+
+print(s.maxProfit([7,1,5,3,6,4]))
+print(s.maxProfit([1,2,3,4,5]))
+print(s.maxProfit([7,6,4,3,1]))
+print(s.maxProfit([6,1,3,2,4,7]))
